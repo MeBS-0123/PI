539c5b9eb37a1e649616c9ce8a39476a6c8f40b7
Romulozz <foxes71502@gmail.com>
2023-06-08 00:33:32 -0500

    b

--- js/carrito.js
+++ js/carrito.js
@@ -1,10 +1,53 @@
-class Carrito{
+class Carrito {
     //Anadir el producto al carrito
-    comprarProducto(e){
+    comprarProducto(e) {
         e.preventDefault();
-        if(e.target.classlist.contains('agregar-carrito')){
-            const product = e.target.parentElement.parentElement;
+        if (e.target.classList.contains('agregar-carrito')) {
+            const producto = e.target.parentElement.parentElement;
             this.leerDatosProductos(producto);
         }
     }
+    leerDatosProductos(producto){
+        const infoProducto = {
+            imagen : producto.querySelector('img').src,
+            titulo : producto.querySelector('h4').textContent,
+            precio : producto.querySelector('.precio span').textContent,
+            id : producto.querySelector('a').getAttribute('data-id'),
+            cantidad : 1
+        }
+        this.insertarCarrito(infoProducto);
     }
+
+    insertarCarrito(producto){
+        const row = document.createElement('tr');
+        row.innerHTML = `
+        <td>
+            <img src="${producto.imagen}" width=100>
+        </td>
+        <td>${producto.titulo}</td>
+        <td>${producto.precio}</td>
+        <td>
+            <a href="#" class="borrar-producto fas fa-times-circle" data-id="${producto.id}"></a>
+        </td>
+        `;
+        listaProductos.appendChild(row);
+    }
+
+    eliminarProducto(e){
+        e.preventDefault();
+        let producto, productoID;
+        if (e.target.classList.contains('borrar-producto')){
+            e.target.parentElement.parentElement.remove();
+            producto = e.target.parentElement.parentElement;
+            productoID = producto.querySelector('a').getAttribute('data-id');
+        }
+    }
+
+    vaciarCarrito(e){
+        e.preventDefault();
+        while (listaProductos.firstChild){
+            listaProductos.removeChild(listaProductos.firstChild);
+        }
+        return false;
+    }
+}
